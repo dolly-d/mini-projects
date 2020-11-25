24129c67744cae87e9a546e00c83a1f8a50611fe
dolly-d <dolly.desir@gmail.com>
2020-11-25 16:44:07 -0500

    added commit

---
+++ tipcalculator/src.js
@@ -0,0 +1,8 @@
+function calculateTip(){
+    let billAmt = document.querySelector(".billAmt").value 
+    let service = document.querySelector(".serviceSelector").value 
+}
+
+document.getElementById("calculate").addEventListener("click", function(){
+    calculateTip()
+})
