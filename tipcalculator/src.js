e783f843d4a28a1e1c6ee44264242f542bbfd2cb
dolly-d <dolly.desir@gmail.com>
2020-12-12 18:16:53 -0500

    added commit

--- tipcalculator/src.js
+++ tipcalculator/src.js
@@ -1,20 +1,20 @@
 function calculateTip(){
     let billAmt = document.querySelector(".billAmt").value 
-    let service = document.querySelector(".serviceSelector").value 
+    let service = document.querySelector("#serviceSelector").value 
     let numOfPeople = document.querySelector(".peopleAmt").value
 
-    if(billAmt === "" || service===0){
+    if(billAmt === "" || service==0){
         alert("please enter a value")
         return
     }
-     if(numofPeople === "" || numOfPeople<= 1){
+     if(numOfPeople === "" || numOfPeople<= 1){
          numOfPeople = 1
          document.getElementById("each").style.display="none"
      } else {
          document.getElementById("each").style.display = "block"
      }
 
-     let total = (billAmt * service) / numofPeople
+     let total = (billAmt * service) / numOfPeople
      total = Math.round(total * 100) / 100
      total = total.toFixed(2)
 
@@ -23,6 +23,7 @@ function calculateTip(){
 }
 
 document.getElementById("totalTip").style.display = "none"
+document.getElementById("each").style.display = "none"
 
 document.getElementById("calculate").addEventListener("click", function(){
     calculateTip()
