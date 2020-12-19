621397606b730d124f6c4c35b231e0d0352c1583
dolly-d <dolly.desir@gmail.com>
2020-12-19 15:32:41 -0500

    update

--- doodlebird/app.js
+++ doodlebird/app.js
@@ -17,6 +17,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
         constructor(newPlatformBottom){
             this.bottom = newPlatformBottom
             this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
         }
     }
 
