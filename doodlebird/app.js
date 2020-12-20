8819c96db2017aea01e3c1a63c8ca9d51db59705
dolly-d <dolly.desir@gmail.com>
2020-12-20 18:02:04 -0500

    update

--- doodlebird/app.js
+++ doodlebird/app.js
@@ -102,19 +102,45 @@ document.addEventListener('DOMContentLoaded', ()=>{
         if (e.key === 'ArrowLeft'){
             moveLeft()
         } else if (e.key === 'ArrowRight'){
-            //moveright
+            moveRight()
         }else if (e.key === 'ArrowUp'){
-            //straightenout
+            straightenOut()
         }
     }
 
     moveLeft=()=>{
+        if (isGoingRight){
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
         isGoingLeft = true 
         leftTimerId = setInterval(function () {
-            doodlerLeftSpace -= 5
-            doodler.style.left = doodlerLeftSpace + 'px'
+            if (doodlerLeftSpace >= 0){
+                doodlerLeftSpace -= 5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveRight()
 
         },30)
+    }
+    moveRight=()=>{
+        if (isGoingLeft){
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+        isGoingRight = true
+        rightTimerId = setInterval(function () {
+            if (doodlerLeftSpace <= 340){
+                doodlerLeftSpace += 5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveLeft()
+
+        },30)
+    }
+    straightenOut=()=>{
+        isGoingRight = false
+        isGoingLeft = false
+        clearInterval(rightTimerId)
+        clearInterval(leftTimerId)
     }
 
     start =()=>{
